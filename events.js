eaa84ce68b7c95bce3ff80c3d63eb33c533b099a
akaki2998 <95980666+akaki2998@users.noreply.github.com>
2022-02-17 21:29:56 +0400

    Add files via upload

---
+++ events.js
@@ -0,0 +1,190 @@
+let Man50Free = {
+    'event' : '50 თავისუფალი',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman50Free = {
+    'event' : '50 თავისუფალი',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+
+let Man100Free = {
+    'event' : '100 თავისუფალი',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman100Free = {
+    'event' : '100 თავისუფალი',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+let Man200Free = {
+    'event' : '200 თავისუფალი',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman200Free = {
+    'event' : '200 თავისუფალი',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+
+let Man400Free = {
+    'event' : '400 თავისუფალი',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman400Free = {
+    'event' : '400 თავისუფალი',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+let Man800Free = {
+    'event' : '800 თავისუფალი',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman800Free = {
+    'event' : '800 თავისუფალი',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+
+let Man1500Free = {
+    'event' : '1500 თავისუფალი',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman1500Free = {
+    'event' : '1500 თავისუფალი',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+
+
+let Man50Breast = {
+    'event' : '50 ბრასი',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman50Breast = {
+    'event' : '50 ბრასი',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+
+let Man100Breast = {
+    'event' : '100 ბრასი',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman100Breast = {
+    'event' : '100 ბრასი',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+let Man200Breast = {
+    'event' : '200 ბრასი',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman200Breast = {
+    'event' : '200 ბრასი',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+let Man50Back = {
+    'event' : '50 გულაღმა',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman50Back = {
+    'event' : '50 გულაღმა',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+
+let Man100Back = {
+    'event' : '100 გულაღმა',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman100Back = {
+    'event' : '100 გულაღმა',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+let Man200Back = {
+    'event' : '200 გულაღმა',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman200Back = {
+    'event' : '200 გულაღმა',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+let Man50Fly = {
+    'event' : '50 ბატერფლაი',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman50Fly = {
+    'event' : '50 ბატერფლაი',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+
+let Man100Fly = {
+    'event' : '100 ბატერფლაი',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman100Fly = {
+    'event' : '100 ბატერფლაი',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+let Man200Fly = {
+    'event' : '200 ბატერფლაი',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman200Fly = {
+    'event' : '200 კომპლექსი',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+let Man100IM = {
+    'event' : '100 კომპლექსი',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman100IM = {
+    'event' : '100 კომპლექსი',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+let Man200IM = {
+    'event' : '200 კომპლექსი',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman200IM = {
+    'event' : '200 კომპლექსი',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+let Man400IM = {
+    'event' : '400 კომპლექსი',
+    'gender' : 'კაცი',
+    'swimmer' : [],
+}
+let Woman400IM = {
+    'event' : '400 კომპლექსი',
+    'gender' : 'ქალი',
+    'swimmer' : [],
+}
+
+let eventsArray = [Man50Free,Woman50Free,Man100Free,Woman100Free,Man200Free,Woman200Free,Man400Free,Woman400Free,Man800Free,Woman800Free,Man1500Free,Woman1500Free,Man50Breast,Woman50Breast,Man100Breast,Woman100Breast,Man200Breast,Woman200Breast,Man50Back,Woman50Back,Man100Back,Woman100Back,Man200Back,Woman200Back,Man50Fly,Woman50Fly,Man100Fly,Woman100Fly,Man200Fly,Woman200Fly,Man100IM,Woman100IM,Man200IM,Woman200IM,Man400IM,Woman400IM]
